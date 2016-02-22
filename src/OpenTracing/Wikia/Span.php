abedd5841ce38a1674e1fd68d6fce86f742d7cde
Władysław Bodzek <wladek@wikia-inc.com>
2016-02-22 22:11:48 +0100

    Rename {set,get}Baggage() to {set,get}BaggageItem()

--- src/OpenTracing/Wikia/Span.php
+++ src/OpenTracing/Wikia/Span.php
@@ -105,7 +105,7 @@ class Span extends OpenTracing\Span
     }
 
     /**
-     * Stores Baggage in the span as a key/value pair.
+     * Stores Baggage Item in the span as a key/value pair.
      *
      * Enables powerful distributed context propagation functionality where
      * arbitrary application data can be carried along the full path of
@@ -124,7 +124,7 @@ class Span extends OpenTracing\Span
      * @param mixed $value
      * @return $this
      */
-    public function setBaggage($key, $value)
+    public function setBaggageItem($key, $value)
     {
         $this->data->baggage[$key] = $value;
 
@@ -132,7 +132,7 @@ class Span extends OpenTracing\Span
     }
 
     /**
-     * Retrieves value of the Baggage with the given key.
+     * Retrieves value of the Baggage Item with the given key.
      *
      * Returns null if key doesn't exist.
      *
@@ -141,7 +141,7 @@ class Span extends OpenTracing\Span
      * @param string $key
      * @return mixed
      */
-    public function getBaggage($key)
+    public function getBaggageItem($key)
     {
         return array_key_exists($key, $this->data->baggage) ? $this->data->baggage[$key] : null;
     }
