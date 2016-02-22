c8308321ba9d3661ae4fbadde333416edfa48af3
Władysław Bodzek <wladek@wikia-inc.com>
2016-02-22 16:30:23 +0100

    Copy Basic implementation

---
+++ src/OpenTracing/Wikia/Format.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace OpenTracing\Wikia;
+
+class Format
+{
+
+    const PACKED_HTTP_HEADERS = 'packed_http_headers';
+    const RAW_HTTP_HEADERS = 'raw_http_headers';
+
+}
---
+++ src/OpenTracing/Wikia/Recorder.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace OpenTracing\Wikia;
+
+interface Recorder
+{
+
+}
